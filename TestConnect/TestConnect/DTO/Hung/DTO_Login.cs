517906b3cc5f08373d3f00324c7340bed6442b61
20127176-DoTungGiaHung <115985429+20127176-DoTungGiaHung@users.noreply.github.com>
2023-11-30 15:43:08 +0700

    Update DTO

---
+++ TestConnect/TestConnect/DTO/Hung/DTO_Login.cs
@@ -0,0 +1,8 @@
+﻿namespace _20HTTT_1.DTO.Hung
+{
+    public class DTO_Login
+    {
+        public string User_Name { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TestConnect/TestConnect/DTO/Hung/DTO_Login_Result.cs
@@ -0,0 +1,7 @@
+﻿namespace _20HTTT_1.DTO.Hung
+{
+    public class DTO_Login_Result
+    {
+        public Boolean OnSuccess { get; set; }  
+    }
+}

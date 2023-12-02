19710111d307dacc2d76a02dc1be35da708714fb
Nhanmaster21 <87769301+Nhanmaster21@users.noreply.github.com>
2023-12-02 16:03:06 +0700

    Add files via upload

---
+++ TestConnect/TestConnect/DTO/Nhan/DTO_Policies.cs
@@ -0,0 +1,11 @@
+﻿namespace _20HTTT_1.DTO.Nhan
+{
+    public class DTO_Policies
+{
+    public string Name { get; set; }
+    public int MinimumAge { get; set; }
+    public int MaximumAge { get; set; }
+    public string Description { get; set; }
+    public long MonthlyPay { get; set; }
+}
+}
---
+++ TestConnect/TestConnect/DTO/Nhan/DTO_Policies_Result.cs
@@ -0,0 +1,12 @@
+﻿namespace _20HTTT_1.DTO.Nhan
+{
+    public class DTO_Policies_Result
+{
+    public Guid IdChinhSach { get; set; }
+    public string Name { get; set; }
+    public int MinimumAge { get; set; }
+    public int MaximumAge { get; set; }
+    public string Description { get; set; }
+    public long MonthlyPay { get; set; }
+}
+}

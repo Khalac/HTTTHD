af6a7011580718028eb79e6e47ded7d790f4637d
20127176-DoTungGiaHung <115985429+20127176-DoTungGiaHung@users.noreply.github.com>
2023-12-03 08:44:50 +0700

    update login Api 1.1

--- TestConnect/TestConnect/Controllers/Hung/API_Login.cs
+++ TestConnect/TestConnect/Controllers/Hung/API_Login.cs
@@ -1,11 +1,12 @@
 ﻿using _20HTTT_1.DTO.Hung;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TestConnect.Data;
 
 namespace _20HTTT_1.Controllers.Hung
 {
-    [Route("api/{controller}")]
+    [Route("api/[controller]")]
     [ApiController]
     public class API_Login : ControllerBase
     {
@@ -24,12 +25,18 @@ namespace _20HTTT_1.Controllers.Hung
             result.OnSuccess = true;
 
             var Check_UserName_Password = healthCareDBContext.User_Login.FirstOrDefault(x => x.UserName == Acount.User_Name && x.Password == Acount.Password);
+            
             if (Check_UserName_Password == null) {
                 result.OnSuccess = false;
+                result.UserId = "No";
                 return Ok(result);
             }
 
+            var UserLoginId = Check_UserName_Password.Id.ToString();
+            var Get_UserID = healthCareDBContext.Khach_Hang.FromSqlRaw("SELECT * FROM Khach_Hang WHERE User_Login_id = {0}", UserLoginId).ToList();
+
             result.OnSuccess = true;
+            result.UserId = Get_UserID[0].Id_Khach.ToString();
             return Ok(result);
         }
 

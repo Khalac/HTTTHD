4d408a758438146b14323ff22032460dd1b59e93
SentoGGBK <115984214+SentoGGBK@users.noreply.github.com>
2023-12-06 11:08:34 +0700

    Add files via upload

---
+++ TestConnect/TestConnect/DTO/Khang/DTO_Policy.cs
@@ -0,0 +1,12 @@
+﻿namespace _20HTTT_1.DTO.Khang
+{
+    public class DTO_Policy
+{
+    public string Name { get; set; }
+    public int MinimumAge { get; set; }
+    public int MaximumAge { get; set; }
+    public string Description { get; set; }
+    public long MonthlyPay { get; set; }
+}
+
+}

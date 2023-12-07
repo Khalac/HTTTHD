0bec6097c35ebb30890725b442ee80e4cb9bca51
tonphuongoanh <81601245+tonphuongoanh@users.noreply.github.com>
2023-12-08 05:22:15 +0700

    Add files via upload

---
+++ TestConnect/TestConnect/DTO/Oanh/PolicyDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace _20HTTT_1.DTO.Oanh
+{
+    public class PolicyDTO
+    {
+        public string Name { get; set; }
+        public int MinimumAge { get; set; }
+        public int MaximumAge { get; set; }
+        public string Description { get; set; }
+        public long MonthlyPay { get; set; }
+    }
+}
---
+++ TestConnect/TestConnect/DTO/Oanh/PolicyResultDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace _20HTTT_1.DTO.Oanh
+{
+    public class PolicyResultDTO
+    {
+        public Guid IdChinhSach { get; set; }
+        public string Name { get; set; }
+        public int MinimumAge { get; set; }
+        public int MaximumAge { get; set; }
+        public string Description { get; set; }
+        public long MonthlyPay { get; set; }
+    }
+}

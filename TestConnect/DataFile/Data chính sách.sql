579e2bf215c149ae122b6e14f560231a136d1896
SentoGGBK <115984214+SentoGGBK@users.noreply.github.com>
2023-12-08 13:29:22 +0700

    data chính sách

---
+++ TestConnect/DataFile/Data chính sách.sql
@@ -0,0 +1,22 @@
+SELECT TOP (1000) [Id_ChinhSach]
+      ,[Name]
+      ,[MinimumAge]
+      ,[MaximumAge]
+      ,[Description]
+      ,[MonthlyPay]
+  FROM [HealthCareDB].[dbo].[Chinh_Sach]
+
+  
+
+insert into Chinh_Sach values ('81fe11b0-57ca-4469-8035-d1e4f12921cb','Bao Hiem A+',1,20,'Cap nhat chinh sach premium',150000)
+insert into Chinh_Sach values ('1A6E8E72-1F59-4B7F-8FFD-EB58CBFA8A4E','Bao Hiem A',1,20,'Bao hiem cho tre em va thieu nien',150000)
+insert into Chinh_Sach values ('79374D2D-B8EB-4C82-A706-745D3A2AAA95','Bao Hiem B',18,30,'Bao hiem cho vi thanh nien',400000)
+insert into Chinh_Sach values ('2aff92b5-a3d8-4b0d-82ed-134909b6ca63','Bao Hiem B+',18,30,'Cap nhat chinh sach premium',450000)
+insert into Chinh_Sach values ('092286dc-9b7d-4180-a261-39b1345c42c7','Bao Hiem C',31,50,'Bao hiem cho thanh nien',500000)
+insert into Chinh_Sach values ('7e65a005-7813-4809-bca1-78cb6ea8063e','Bao Hiem C+',31,50,'Cap nhat chinh sach premium',550000)
+insert into Chinh_Sach values ('5c42f4e4-709a-4384-b7d6-9c8664495ea5','Bao Hiem D',51,80,'Bao hiem cho trung nien',400000)
+insert into Chinh_Sach values ('bed3b007-8207-42a7-b3f2-a19ada78683e','Bao Hiem D+',51,80,'Cap nhat chinh sach premium',450000)
+insert into Chinh_Sach values ('0d4ce089-e28c-4f33-a104-3e5a8ced3276','Bao Hiem E',81,100,'Bao hiem cho nguoi lon tuoi',300000)
+insert into Chinh_Sach values ('eef2c304-c011-4a71-a789-9dc8291d3dbd','Bao Hiem E+',81,100,'Cap nhat chinh sach premium',350000)
+insert into Chinh_Sach values ('71a7179a-3e49-4172-bfbc-c10c0c28f460','Bao Hiem A Pro Max',1,20,'Bao hiem pro max cho tre em va thieu nien',400000)
+insert into Chinh_Sach values ('fac6071a-d3d2-4f49-b453-ffe3598b2e38','Bao Hiem B Pro Max',18,30,'Bao hiem pro max cho vi thanh nien',650000)

69881a860283ae69e0003708ee95fc8a7960caf0
khalac <dinhanhquoc2525@gmail.com>
2023-12-14 17:20:50 +0700

    Update index.js

--- fe/src/index.js
+++ fe/src/index.js
@@ -6,6 +6,7 @@ import MainPage_KH from "./pages/MainPage_KH/MainPage_KH";
 import MainPage_NV from "./pages/MainPage_NV/MainPage_NV";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import ChinhSuaCS from "./pages/ChinhSuaCS/ChinhSuaCS";
+import RegisterForm from "./components/RegisterForm/RegisterForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,6 +14,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route exac path="/" element={<LoginPage />} />
+        <Route exac path="/Register" element={<RegisterForm />} />
         <Route exac path="/MainPage_NV" element={<MainPage_NV />} >
 
         </Route>

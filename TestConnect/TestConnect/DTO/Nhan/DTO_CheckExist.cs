4666667abac120e5d184f721b8788f9ebe64bfca
Nhanmaster21 <87769301+Nhanmaster21@users.noreply.github.com>
2023-12-23 16:41:14 +0700

    Add files via upload

---
+++ TestConnect/TestConnect/DTO/Nhan/DTO_CheckExist.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using TestConnect.Domain;
+
+namespace _20HTTT_1.DTO.Nhan
+{
+    public class DTO_CheckExist
+{
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public DateTime BuyDate { get; set; }
+
+        public Guid Id_Khach { get; set; }
+        public Guid Id_ChinhSach { get; set; }
+
+
+    }
+}

806532307e19e64ce642d66946bc520610b76196
SentoGGBK <115984214+SentoGGBK@users.noreply.github.com>
2023-12-24 15:05:09 +0700

    DTO update - Khang

---
+++ TestConnect/TestConnect/DTO/Khang/DTO_UpdateStatus_Accept.cs
@@ -0,0 +1,9 @@
+﻿namespace _20HTTT_1.DTO.Khang
+{
+    public class DTO_UpdateStatus_Accept
+    {
+        public Guid Id {  get; set; }
+        public string Status { get; set; }
+
+    }
+}
---
+++ TestConnect/TestConnect/DTO/Khang/DTO_UpdateStatus_Deny.cs
@@ -0,0 +1,8 @@
+﻿namespace _20HTTT_1.DTO.Khang
+{
+    public class DTO_UpdateStatus_Deny
+    {
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+    }
+}

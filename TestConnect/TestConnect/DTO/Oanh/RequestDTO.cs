8e4c5012bb7b1d2c8a19d12dc0cc4bffd22e24d3
tonphuongoanh <81601245+tonphuongoanh@users.noreply.github.com>
2024-01-09 03:56:25 +0700

    Add files via upload

---
+++ TestConnect/TestConnect/DTO/Oanh/RequestDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace _20HTTT_1.DTO.Oanh
+{
+    public class RequestDTO
+    {
+        public Guid Id { get; set; }
+        public string Status { get; set; }
+        public Guid IdChinhsach { get; set; }
+        public Guid IdKhach { get; set; }
+    }
+}
+
+

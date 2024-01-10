e8b863d3ada669aeb34d94955d75b521320f8e03
20127176-DoTungGiaHung <115985429+20127176-DoTungGiaHung@users.noreply.github.com>
2024-01-10 11:37:20 +0700

    Update get ChinhSach DTO

---
+++ TestConnect/TestConnect/DTO/Hung/DTO_Get_ChinhSach_Info_Input.cs
@@ -0,0 +1,9 @@
+﻿namespace _20HTTT_1.DTO.Hung
+{
+    public class DTO_Get_ChinhSach_Info_Input
+    {
+        public string Status { get; set; }  
+
+
+    }
+}
---
+++ TestConnect/TestConnect/DTO/Hung/DTO_Get_ChinhSach_Info_OutPut.cs
@@ -0,0 +1,14 @@
+﻿namespace _20HTTT_1.DTO.Hung
+{
+    public class DTO_Get_ChinhSach_Info_OutPut
+    {
+        public Guid IdChinhSach { get; set; }
+        public string Name { get; set; }
+        public int MinimumAge { get; set; }
+        public int MaximumAge { get; set; }
+        public string Description { get; set; }
+        public long MonthlyPay { get; set; }
+        public string Status { get; set; }
+
+}
+}

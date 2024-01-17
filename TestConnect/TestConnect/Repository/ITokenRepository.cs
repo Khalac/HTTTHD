389d6bc5b0ed0218dce53975294ac6db03ec258d
20127176-DoTungGiaHung <115985429+20127176-DoTungGiaHung@users.noreply.github.com>
2024-01-17 11:23:21 +0700

    New folder Respository

---
+++ TestConnect/TestConnect/Repository/ITokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace _20HTTT_1.Repository
+{
+    public interface ITokenRepository
+    {
+        string CreateJWTToken(IdentityUser user, List<string> roles);
+    }
+}

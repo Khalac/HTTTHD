c27953c6bd6fb0e8d3a97277da92d334e1cc9b7d
Nhanmaster21 <87769301+Nhanmaster21@users.noreply.github.com>
2024-01-18 20:42:24 +0700

    Add files via upload

---
+++ TestConnect/TestConnect/DTO/Nhan/DTO_ViewInfoEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace _20HTTT_1.DTO.Nhan
+{
+    public class DTO_ViewInfoEmployee
+{
+    public Guid Id_NhanVien { get; set; }
+    public string Full_Name { get; set; }
+    public DateTime Birhday { get; set; }
+    public string Gender { get; set; }
+    public string Adress { get; set; }
+}
+}

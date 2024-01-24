f8e480032b1c1769eb451ad5bc378e00f1ebcd0b
20127176-DoTungGiaHung <115985429+20127176-DoTungGiaHung@users.noreply.github.com>
2024-01-24 10:57:13 +0700

    add name and description

--- TestConnect/TestConnect/DTO/Oanh/RequestDTO.cs
+++ TestConnect/TestConnect/DTO/Oanh/RequestDTO.cs
@@ -6,6 +6,8 @@
         public string Status { get; set; }
         public Guid IdChinhsach { get; set; }
         public Guid IdKhach { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
 
